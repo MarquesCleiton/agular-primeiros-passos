7477d25d8d0a7f0dacd590518f570621b097687a
marquescleiton <cleiton.contato2210@gmail.com>
2022-11-15 21:23:21 -0300

    documentação

--- primeiro_projeto_angular/src/app/app.module.ts
+++ primeiro_projeto_angular/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TituloComponent } from './titulo/titulo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule
